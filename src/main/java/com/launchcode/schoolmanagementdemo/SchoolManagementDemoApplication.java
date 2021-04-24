c3ee101a1e9909d107635875368d4ff0354402fb
malathi416 <ammu.416@gmail.com>
2021-04-24 15:46:16 -0500

    Initial commit

---
+++ src/main/java/com/launchcode/schoolmanagementdemo/SchoolManagementDemoApplication.java
@@ -0,0 +1,13 @@
+package com.launchcode.schoolmanagementdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/launchcode/schoolmanagementdemo/SchoolManagementDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launchcode.schoolmanagementdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
