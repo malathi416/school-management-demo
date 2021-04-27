d106cb81ea7fc4166028adb2abbf347fd8a484cb
malathi416 <ammu.416@gmail.com>
2021-04-27 00:58:00 -0500

    delete-class-page completed with bootstrap

--- src/main/java/com/launchcode/schoolmanagementdemo/controllers/ClassController.java
+++ src/main/java/com/launchcode/schoolmanagementdemo/controllers/ClassController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import java.util.Optional;
@@ -53,15 +54,17 @@ public class ClassController {
 
 
     @PostMapping("showFormForUpdate")
-    public String processEditForm(@RequestParam(required = false) Integer id,@ModelAttribute Classes editClass) {
+    public String processEditForm(@RequestParam(required = false) Integer id,@ModelAttribute Classes editClass,final RedirectAttributes redirectAttributes) {
+//        redirectAttributes.addFlashAttribute("message", "updated the class record Success fully !!");
         classesRepository.save(editClass);
+
         return "redirect:";
     }
-//    @GetMapping("/delete/{id}")
-//    public String deleteEventById( @PathVariable(value="id") Integer id) {
-//        classesRepository.deleteById(id);
-//        return "redirect:/";
-//
-//    }
+    @GetMapping("/deleteClass/{id}")
+    public String deleteEventById(@PathVariable(value="id") Integer id, Model model, RedirectAttributes redirectAttributes) {
+        classesRepository.deleteById(id);
+        redirectAttributes.addFlashAttribute("success", "Deleted the class record Success fully where  ID = "+ id +"");
+        return "redirect:/classes";
+    }
 
 }
