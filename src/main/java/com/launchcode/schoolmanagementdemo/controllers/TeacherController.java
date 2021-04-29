30fe9a1b0e402f1a399591b7b3db276dc9e17be3
malathi416 <ammu.416@gmail.com>
2021-04-29 16:07:33 -0500

    completed the edit teacher details page

--- src/main/java/com/launchcode/schoolmanagementdemo/controllers/TeacherController.java
+++ src/main/java/com/launchcode/schoolmanagementdemo/controllers/TeacherController.java
@@ -2,18 +2,18 @@ package com.launchcode.schoolmanagementdemo.controllers;
 
 import com.launchcode.schoolmanagementdemo.data.ClassesRepository;
 import com.launchcode.schoolmanagementdemo.data.TeacherRepository;
+import com.launchcode.schoolmanagementdemo.models.Classes;
 import com.launchcode.schoolmanagementdemo.models.Gender;
 import com.launchcode.schoolmanagementdemo.models.Teacher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("teachers")
@@ -49,6 +49,23 @@ public class TeacherController {
             return "teacher/newTeacherForm";
         }
         teacherRepository.save(teacher);
+        return "redirect:";
+    }
+    @GetMapping("showFormForUpdate/{id}")
+    public String displayTeacherEditForm(Model model, @PathVariable(value="id") Integer id) {
+        Optional<Teacher> result = teacherRepository.findById(id);
+        Teacher editTeacher = result.get();
+        model.addAttribute("title","Update  Teacher  where  ID = "+id +" name is "+ editTeacher.getPersonalDetails().getFirstName()) ;
+        model.addAttribute("teacher",editTeacher);
+        model.addAttribute("genders", Gender.values());
+        model.addAttribute("classesToTeach",classesRepository.findAll());
+        return "teacher/edit-teacher-page";
+    }
+    @PostMapping("showFormForUpdate")
+    public String processTeacherEditedForm(@RequestParam(required = false) Integer id,@ModelAttribute Teacher editTeacher,final RedirectAttributes redirectAttributes) {
+//        redirectAttributes.addFlashAttribute("message", "updated the class record Success fully !!");
+        teacherRepository.save(editTeacher);
+
         return "redirect:";
     }
 }
